3a3243e7f2086656fd8f11f87927f04a167523c6
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-10-28 23:52:21 +0100

    adds loading and handles wowhead api differently

--- src/views/players/show.svelte
+++ src/views/players/show.svelte
@@ -2,63 +2,83 @@
   import { onMount } from "svelte";
   import { location } from "svelte-spa-router";
   import { get } from "@/lib/axios";
-  import axios from 'axios';
+  import axios from "axios";
+  import Loading from "@/components/Loading.svelte";
+  import {INVENTORY_ITEMS, formatDate} from '@/globals'
   let player = {
     name: ""
   };
-  let playerGear = {};
 
+  let WOWHEAD_ITEM_URL = "https://classic.wowhead.com/tooltip/item/";
+  const itemColors = {
+    0: "#9d9d9d",
+    1: "#fff",
+    2: "#1eff00",
+    3: "#0070dd",
+    4: "#a335ee",
+    5: "#ff8000"
+  };
   const getPlayerData = async () => {
-    const {data} = await get({ url: $location });
-    // const test = await axios.get('')
-		if (data) {
+    const { data } = await get({ url: $location });
+    let gear = [];
+    for (const key of Object.keys(data.playerGear)) {
+      if (data.playerGear[key]) {
+        const item = await axios.get(
+          `${WOWHEAD_ITEM_URL}${data.playerGear[key]}`
+        );
+        gear.push({
+          slot: INVENTORY_ITEMS[key],
+          id: data.playerGear[key],
+          name: item.data.name,
+          quality: item.data.quality,
+          color: itemColors[item.data.quality]
+        });
+      }
+    }
+    data["gear"] = gear;
+    if (data) {
       return data;
-		} else {
-      throw new Error(text);
-		}
-  }
+    } else {
+      throw new Error("text");
+    }
+  };
 
   let promise = getPlayerData();
-
-  let keys = [
-    "slot_1",
-    "slot_2",
-    "slot_3",
-    "slot_4",
-    "slot_5",
-    "slot_6",
-    "slot_7",
-    "slot_8",
-    "slot_9",
-    "slot_10",
-    "slot_11",
-    "slot_12",
-    "slot_13",
-    "slot_14",
-    "slot_15",
-    "slot_16",
-    "slot_17",
-    "slot_18",
-    "slot_19"
-  ];
 </script>
 
 <style lang="stylus">
+  .info
 
+    span
+      display: flex
+
+    h1
+      span
+        font-size: 0.5em
 </style>
 
+
+<div class="content">
 {#await promise}
-  waiting
+  <Loading />
 {:then data}
-<h1>{data.player.name}</h1>
+  <div class="info">
+    <h1>{data.player.name} {data.player.race} <span>{data.player.guildRank} of {`<${data.player.guild}>`}</span></h1>
+    <span></span>
+    <span>Last seen by {data.player.uploader} at:</span>
+    <span>{formatDate(data.player.lastSeen)}</span>
+  </div>
   <ul>
-    {#each keys as key}
+    {#each data.gear as i}
       <li>
-        <a href="#"  data-wowhead={`item=${data.playerGear[key]}&domain=classic`}>#</a>
+          <span>{i.slot}:</span>
+        <a href="#" data-wowhead={`item=${i.id}&domain=classic`}>
+          <span style={`color:${i.color}`}>{i.name}</span>
+        </a>
       </li>
     {/each}
   </ul>
 {:catch}
   nej
 {/await}
-
+</div>
