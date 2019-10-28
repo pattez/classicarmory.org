8341f880213c050df5560fdc6293a8b6e0949fdb
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-10-28 23:50:53 +0100

    adds global INVENTORY_ITEMS

--- src/globals/index.js
+++ src/globals/index.js
@@ -20,4 +20,26 @@ const formatDate = d => {
   )}`;
 };
 
-export {BACKEND_URL, genders, zerolize, formatDate};
+const INVENTORY_ITEMS = {
+  'slot_1': "Head",
+  'slot_2': "Neck",
+  'slot_3': "Shoulders",
+  'slot_4': "Shirt",
+  'slot_5': "Chest",
+  'slot_6': "Waist",
+  'slot_7': "Legs",
+  'slot_8': "Feet",
+  'slot_9': "Wrists",
+  'slot_10': "Hands",
+  'slot_11': "Ring 1",
+  'slot_12': "Ring 2",
+  'slot_13': "Trinket 1",
+  'slot_14': "Trinket 2",
+  'slot_15': "Back",
+  'slot_16': "Main hand",
+  'slot_17': "Off hand",
+  'slot_18': "Ranged",
+  'slot_19': "Tabard"
+};
+
+export { BACKEND_URL, genders, zerolize, formatDate, INVENTORY_ITEMS };
