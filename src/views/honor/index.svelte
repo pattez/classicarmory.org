a389386d9feefa519cb4f3bffda4af92a7f0f3f1
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-11-17 01:50:24 +0100

    wip honor/index.svelte

--- src/views/honor/index.svelte
+++ src/views/honor/index.svelte
@@ -1,62 +1,164 @@
 <script>
   import Table from '@/components/Table.svelte';
   import Loading from '@/components/Loading.svelte';
+  import Dropdown from '@/components/Dropdown.svelte';
   import {axiosPost} from '@/lib/axios';
+  import {formatDate, SERVERS, FRONTEND_URL} from '@/globals'
+  import { onMount } from 'svelte';
+
   const columns = [
+    {name: "", key: "characterSrc", img:true},
+    {name: "", key: "classSrc", img: true },
+    {name: "", key: "rankSrc", img: true },
     {name: 'Name', key: 'name'},
     {name: 'Guild', key: 'guild'},
     {name: 'Guild rank', key: 'guildRank'},
-    {name: 'Race', key: 'race'},
-    {name: 'Gender', key: 'gender'},
-    {name: 'Class', key: 'class'},
     {name: 'Last seen', key: 'lastSeen'},
+    {name: 'Last week standing', key: 'lastweekStanding'},
     {name: 'HKs today', key: 'todayHK'},
-    {name: 'Honor today', key: 'todayHonor'},
     {name: 'HKs yesterday', key: 'yesterdayHK'},
     {name: 'Honor yesterday', key: 'yesterdayHonor'},
+    {name: 'This week HKs', key: 'thisweekHK'},
+    {name: 'This week honor', key: 'thisweekHonor'},
     {name: 'Lifetime HKs', key: 'lifetimeHK'},
-    {name: 'Lifetime Rank', key: 'lifetimeRank'},
-    {name: 'Honor progress', key: 'honorProgress'},
-    {name: 'Rank', key: 'rankNumber'},
   ];
 
-  const get = async () => {
+  let orderBys = [
+    {name: 'Last seen', key: 'lastSeen'},
+    {name: 'Last week standing', key: 'lastweekStanding'},
+    {name: 'HKs today', key: 'todayHK'},
+    {name: 'HKs yesterday', key: 'yesterdayHK'},
+    {name: 'Honor yesterday', key: 'yesterdayHonor'},
+    {name: 'This week HKs', key: 'thisweekHK'},
+    {name: 'This week honor', key: 'thisweekHonor'},
+    {name: 'Lifetime HKs', key: 'lifetimeHK'},
+  ];
+
+  let servers = Object.keys(SERVERS).map((i) => {
+    return {
+      id: i,
+      name: SERVERS[i].name,
+      type: SERVERS[i].faction
+    }
+  });
+
+  const factions = [
+    {id: 1, name: "Alliance"},
+    {id: 2, name: "Horde"}
+  ]
+
+  const searchItems = {
+    'orderBy': {name: 'Last week standing', key: 'lastweekStanding'},
+    'faction': {id: 1, name: "Alliance"},
+    'server': {id: 9, name: 'Gehennas'}
+  }
+
+  let showLoadMore = false;
+  let initialOffset = 0;
+  let dataLength = 0;
+  let honorData = [];
+
+  const get = async (offset, join) => {
+    getLocalStorage()
     const {data} = await axiosPost({
       url: '/honor',
       body: {
-      serverId: 9,
-      offset: 0
+      serverId: searchItems.server.id,
+      offset,
+      faction: searchItems.faction.id,
+      orderBy: searchItems.orderBy.key
       }
     });
     if (data) {
-      return data;
+      showLoadMore = data.length >= 50;
+      dataLength = data.length;
+
+      return data.map(i => {
+        return {
+          ...i,
+          lastSeen: formatDate(i.lastSeen),
+          lifetimeRank: i.lifetimeRank || 0,
+          lifetimeHK: i.lifetimeHK || 0,
+          characterSrc: `assets/character/${i.raceId}_${i.genderId}.jpg`,
+          classSrc: `assets/class/${i.classId}.jpg`,
+          rankSrc: i.rankNumber > 0 && i.rankNumber < 14 ? `assets/rank/${i.faction}_${i.rankNumber}` : null
+        }
+      })
     } else {
       throw new Error('text');
     }
   }
 
-  const promise = get();
+  const fetchMore = async () => {
+    if(dataLength > 0) {
+      initialOffset = initialOffset + 50;
+      await get(initialOffset, true)
+    }
+  };
+
+  const getLocalStorage = () => {
+    for(const item of Object.keys(searchItems)) {
+      const value = localStorage.getItem(item)
+      if (value) {
+        searchItems[item] = JSON.parse(value);
+      }
+    }
+  }
+
+  const factionClick = async ({detail}) => {
+    searchItems.faction = detail;
+    setLocalStorage('faction', detail)
+    initialOffset = 0
+    promise = get(initialOffset)
+  }
+
+  const serverClick = async ({detail}) => {
+    searchItems.server = detail
+    setLocalStorage('server', detail)
+    initialOffset = 0
+    promise = get(initialOffset)
+  }
+
+  const orderByClick = async ({detail}) => {
+    searchItems.orderBy = detail;
+    setLocalStorage('orderBy', detail)
+    initialOffset = 0
+    promise = get(initialOffset)
+  }
+
+  const setLocalStorage = (key, item) => {
+    localStorage.setItem(key, JSON.stringify(item));
+  }
+
+  const rowClick = ({detail}) => {
+    window.open(`${FRONTEND_URL}/#/players/${detail.id}`)
+  }
+
+  let promise = get(initialOffset)
 
 </script>
 
 
-<h1>Honor</h1>
 {#await promise}
 <Loading/>
 {:then data}
 <div class="content">
+<h1>Honor</h1>
+<div class="dropdowns">
+  <div class="dropdown factions">
+  <Dropdown items={factions} name={searchItems.faction.name} on:click={factionClick}/>
+  </div>
+  <div class="dropdown servers">
+  <Dropdown items={servers} name={searchItems.server.name} on:click={serverClick}/>
+  </div>
+<div class="dropdown orderBys">
+  <Dropdown items={orderBys} name={searchItems.orderBy.name} on:click={orderByClick}/>
+  </div>
+</div>
   <div class="alliance">
-  <h1>Alliance</h1>
     <div class="table">
-    <Table items={data} {columns}/>
+    <Table items={data} {columns} on:click={rowClick} showLoadMore={showLoadMore} on:load-more={fetchMore}/>
     </div>
-  </div>
-  <div class="horde">
-    <h1>Horde</h1>
-  <div class="table">
-    <Table items={[]} {columns}/>
-
-  </div>
   </div>
 </div>
 {:catch}
@@ -68,14 +170,37 @@
 <style lang="stylus">
 
   .content
-    display: grid
-    grid-template-columns: 1fr
-    grid-template-rows: 1fr 1fr
+    width: calc(100% - 300px)
+    margin: 0 auto
 
   .alliance
     grid-row: 1
 
   .horde
     grid-row: 2
+
+  .dropdown
+
+    &:not(:first-child)
+      margin-left: 15px
+
+    &.servers
+      width: 200px
+
+    &.factions
+      width: 150px
+
+    &.orderBys
+      width: 250px
+
+  .dropdowns
+    display: flex
+    margin-bottom: 20px
+
+  .h1
+    display: flex
+
+  h1:not(:first-child)
+    margin-left: 15px
 
 </style>
