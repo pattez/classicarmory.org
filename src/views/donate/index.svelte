56b37899e841537d18ebf22d2ceebb60d6dc700c
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-11-18 16:53:28 +0100

    Update index.svelte

--- src/views/donate/index.svelte
+++ src/views/donate/index.svelte
@@ -1,5 +1,5 @@
 <script>
-  import {PATREON_URL, DISCORD_URL} from '@/globals';
+  import {PATREON_URL, DISCORD_URL, FRONTEND_URL} from '@/globals';
 
 </script>
 
@@ -8,11 +8,11 @@
   <h1>Donate</h1>
   <div class="donate">
     <div class="info">
-      https://classicarmory.org/ World of Warcraft Classic Armory was created by a sole developer, taking roughly three weeks to develop to this point.
+      World of Warcraft Classic Armory <a href={FRONTEND_URL}>{FRONTEND_URL}</a> was created by a sole developer, taking roughly three weeks to develop to this point.
       In addition to being the sole developer I also financially maintain the website. Any donations towards the running costs of the Classic Armory are greatly appreciated.
-      These costs monthly at the moment are rougly around 50$ USD. In return for your contribution, aside from my thanks, you'll get a shiny rank in the Classic Armory discord. (2019-11-11)
+      These costs monthly at the moment are rougly around 50$ USD. In return for your contribution, aside from my thanks, you'll get a shiny rank in the Classic Armory discord. (written 2019-11-11)
     </div>
-    <span><a href={PATREON_URL}>Patreon</span>
+    <span><a href={PATREON_URL}>Patreon</a></span>
     <span>Other ways of donating will be added eventually, if you feel like donating through paypal or by sending bitcoin or another other cryptocurrency, contact me on discord pattez#4267</span>
   </div>
 </div>
