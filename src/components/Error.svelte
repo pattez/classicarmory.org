e0f62eb24b813dadf52bd7d8d71e1f940089489b
Patrik Olofsson <patrik.d.olofsson@gmail.com>
2019-12-22 20:09:23 +0100

    Create error component

---
+++ src/components/Error.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Loading from "@/components/Loading.svelte";
+</script>
+
+<style lang="stylus">
+  @require 'styles/empty'
+
+  .img
+    animation: rotate 1.5s infinite
+
+  .error
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+    display: flex
+    justify-content: center
+    align-items: center
+
+  .content
+    display: inline-grid
+
+  span, .icon
+    display: flex
+    justify-content: center
+    align-items: center
+
+</style>
+
+<div class="error">
+  <div class="content">
+    <div class="icon">
+      <img src="assets/images/armory.png" alt="background image" class="img" />
+    </div>
+    <span>Something went terribly wrong... </span>
+    <span>Try refreshing the page or contact an admin.</span>
+  </div>
+</div>
